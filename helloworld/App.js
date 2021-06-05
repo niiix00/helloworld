d6f6d92e9799f018d60140e6c317884ec40b53e3
niiix00 <katrinaecot@gmail.com>
2021-06-05 20:29:46 +0800

    first commit

---
+++ helloworld/App.js
@@ -0,0 +1,27 @@
+//BSCS 3-4
+//Ecot, Katrina Jane
+//Bito-on, Prince Carl
+
+import * as React from 'react';
+import { Text, View, StyleSheet, Card } from 'react-native';
+
+export default function App() {
+  return (
+
+             <View style={{
+               flex: 1,
+               backgroundColor: '#AEC6CF',
+               alignItems: 'center',
+               justifyContent: 'center',
+              }}>
+    
+               <Text style={{
+                  color: '#FFFFFF',
+                  fontSize: 42,
+                  fontFamily: 'Felix Titling',
+                }}>
+                   Hello World!
+               </Text>
+              </View>
+  );
+}
